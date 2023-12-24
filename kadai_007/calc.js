7a4ca0ea4f669323270dd1e1b50669c1e35bad9a
SAYA <120891366+sssakurasaya@users.noreply.github.com>
2023-12-24 09:33:51 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+
+//1以上の正の数を代入
+let num = 15;
+
+//条件分岐
+if(num % 3 === 0 && num % 5 === 0) {
+   console.log("3と5の倍数です");
+  }  else if (num % 3 === 0) {
+    console.log("3の倍数です");
+  } else if (num % 5 ===0){
+    console.log("5の倍数です");
+  } else {
+    console.log(num);
+  }
+
+
+
+
